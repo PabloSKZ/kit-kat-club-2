edf84d892d0d51415024cf29162379d5e70e8311
Chelton Ajax <chelton1506@gmail.com>
2020-03-10 19:02:04 +0100

    merge

--- app/controllers/join_table_items_carts_controller.rb
+++ app/controllers/join_table_items_carts_controller.rb
@@ -1,5 +1,12 @@
 class JoinTableItemsCartsController < ApplicationController
   def create
+    @jt = JoinTableItemsCart.create(item_id: params[:id], cart_id: current_user.cart.id, quantity: 1)
+    puts "Ok chef"
+    puts params[:item_id]
+  end
+
+  def create2
+    
   end
 
   def update
