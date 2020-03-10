9a848a9927e6cb392eda27ccf3d72e3e6b434a87
PabloSKZ <pablo.eskenazi@gmail.com>
2020-03-10 12:54:22 +0100

    def modif api

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['MAILER_KEY'],
-  :password => ENV['MAILER_ID'],
+  :user_name => ENV['MAILER_ID'],
+  :password => ENV['MAILER_KEY'],
   :domain => 'kit-kat-club-2.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
