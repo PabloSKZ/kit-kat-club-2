8f7a1298298f7d4a209e3e000a4a13f2b24d9c4a
ArnoTnDev <arnaudtaniere.dev@gmail.com>
2020-03-11 15:13:50 +0100

    create order_table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   has_many :join_table_items_carts, dependent: :nullify
+  has_many :join_table_items_orders
+  has_many :orders, through: :join_table_items_orders
 end
