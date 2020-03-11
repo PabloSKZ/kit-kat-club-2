c3af944a5365c208a0db7759264073cbffc947ff
Chelton Ajax <chelton1506@gmail.com>
2020-03-11 14:39:25 +0100

    fixed bug where adding items to your cart w/o being signed in would crash

--- app/controllers/join_table_items_carts_controller.rb
+++ app/controllers/join_table_items_carts_controller.rb
@@ -1,15 +1,20 @@
 class JoinTableItemsCartsController < ApplicationController
   def create
-    @current_user_cart = current_user.cart.items.ids
-    @item = current_user.cart.join_table_items_carts
-    if @current_user_cart.include?(params[:format].to_i)
-      @current_quantity = @item.find_by(item_id: params[:format]).quantity
-      @current_quantity += 1
-      @item.find_by(item_id: params[:format]).update(quantity: @current_quantity)
+    if current_user == nil
+      flash.alert = "veuillez vous connecter afin d'ajouter des objets a votre panier."
+      redirect_to new_user_session_path
     else
-      @jt = JoinTableItemsCart.create(item_id: params[:format], cart_id: current_user.cart.id, quantity: 1)
+      @current_user_cart = current_user.cart.items.ids
+      @item = current_user.cart.join_table_items_carts
+      if @current_user_cart.include?(params[:format].to_i)
+        @current_quantity = @item.find_by(item_id: params[:format]).quantity
+        @current_quantity += 1
+        @item.find_by(item_id: params[:format]).update(quantity: @current_quantity)
+      else
+        @jt = JoinTableItemsCart.create(item_id: params[:format], cart_id: current_user.cart.id, quantity: 1)
+      end
+      redirect_to items_path
     end
-    redirect_to items_path
   end
 
   def update
