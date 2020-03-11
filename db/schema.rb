4a4224ed0c955066282fb36f2f03c3e0365006e2
Nazaninf24 <nazaninf24@gmail.com>
2020-03-11 16:30:59 +0100

    update psql

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_154618) do
+ActiveRecord::Schema.define(version: 2020_03_10_153545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cart_item_joint_tables", force: :cascade do |t|
-    t.bigint "cart_id"
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_item_joint_tables_on_cart_id"
-    t.index ["item_id"], name: "index_cart_item_joint_tables_on_item_id"
-  end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
