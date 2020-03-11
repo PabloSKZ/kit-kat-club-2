60418393d540793beb1e4a08ba1cba049e5664de
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-03-11 12:35:20 +0100

    price calculation

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   def show
-    @items = current_user.cart.items
+    @cart_subtotal = cart_total_price
   end
 
   def create
@@ -11,4 +11,17 @@ class CartsController < ApplicationController
 
   def destroy
   end
+
+
+  private 
+
+  def cart_total_price
+    @cart = Cart.find(current_user.cart.id)
+    @price = 0
+    @cart.items.each do |i|
+      price += i.price
+    end
+    puts price
+  end
+  
 end
