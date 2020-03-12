55f637b93677f7e6d90f4abeb10115871afdc57e
ArnoTnDev <arnaudtaniere.dev@gmail.com>
2020-03-12 12:30:44 +0100

    admin_field_creation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   has_many :join_table_items_carts, dependent: :nullify
   has_many :join_table_items_orders
   has_many :orders, through: :join_table_items_orders
+  has_one_attached :picture
 end
