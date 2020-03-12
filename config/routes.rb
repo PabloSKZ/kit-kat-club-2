4be11a0af7fc771b500921a5c53816a135808855
Nazaninf24 <nazaninf24@gmail.com>
2020-03-12 13:12:35 +0100

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :items
-<<<<<<< HEAD
   resources :charges
-=======
-  resources :orders
->>>>>>> show-order
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
